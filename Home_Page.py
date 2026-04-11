314b531c7fba9c5de37ba7ca9bafc32c071d791b
davidrlhilljr-cmyk <davidrlhilljr@gmail.com>
2026-04-10 20:58:53 -0400

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -16,7 +16,7 @@ st.subheader("David Hill, Edward Chadwick")
 #       1. **Page Name**: Description
 #       2. **Page Name**: Description
 #       3. **Page Name**: Description
-#       4. **Page Name**: Description
+
 
 st.write("""
 Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
